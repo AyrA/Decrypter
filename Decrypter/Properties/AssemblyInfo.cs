b60a531bf5fbe3c7b8ecaab14c4445bf8fbd6c65
Kevin Gut <kevin.gut@bitm.sg>
2017-07-23 12:21:25 +0200

    Set binary properties properly

--- Decrypter/Properties/AssemblyInfo.cs
+++ Decrypter/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Decrypter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Decrypts DLC files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("/u/AyrA_ch")]
 [assembly: AssemblyProduct("Decrypter")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © /u/AyrA_ch 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
