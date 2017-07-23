397f3a0040eb584bd378a1d82347bc44a8ae5cb1
Kevin Gut <kevin.gut@bitm.sg>
2017-07-23 12:25:53 +0200

    Basic form

---
+++ Decrypter/frmMain.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Decrypter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.tbSave = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.tbLinks = new System.Windows.Forms.TextBox();
+            this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.SFD = new System.Windows.Forms.SaveFileDialog();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(531, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "&Open...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // tbSave
+            // 
+            this.tbSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSave.Location = new System.Drawing.Point(531, 41);
+            this.tbSave.Name = "tbSave";
+            this.tbSave.Size = new System.Drawing.Size(75, 23);
+            this.tbSave.TabIndex = 3;
+            this.tbSave.Text = "&Save...";
+            this.tbSave.UseVisualStyleBackColor = true;
+            this.tbSave.Click += new System.EventHandler(this.tbSave_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput.Location = new System.Drawing.Point(12, 14);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.ReadOnly = true;
+            this.tbInput.Size = new System.Drawing.Size(513, 20);
+            this.tbInput.TabIndex = 0;
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOutput.Location = new System.Drawing.Point(12, 43);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.Size = new System.Drawing.Size(513, 20);
+            this.tbOutput.TabIndex = 2;
+            // 
+            // tbLinks
+            // 
+            this.tbLinks.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLinks.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLinks.Location = new System.Drawing.Point(12, 69);
+            this.tbLinks.Multiline = true;
+            this.tbLinks.Name = "tbLinks";
+            this.tbLinks.ReadOnly = true;
+            this.tbLinks.Size = new System.Drawing.Size(594, 326);
+            this.tbLinks.TabIndex = 4;
+            // 
+            // OFD
+            // 
+            this.OFD.DefaultExt = "dlc";
+            this.OFD.Filter = "Link container|*.dlc;*.rsdf;*.ccf";
+            this.OFD.Title = "Select link container";
+            // 
+            // SFD
+            // 
+            this.SFD.DefaultExt = "txt";
+            this.SFD.Filter = "Text files|*.txt";
+            this.SFD.Title = "Save unencrypted link list";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDecrypt.Location = new System.Drawing.Point(450, 401);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "&Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopy.Location = new System.Drawing.Point(531, 401);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "&Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 432);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.tbLinks);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.tbSave);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "frmMain";
+            this.Text = "DLC Decrypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button tbSave;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.TextBox tbLinks;
+        private System.Windows.Forms.OpenFileDialog OFD;
+        private System.Windows.Forms.SaveFileDialog SFD;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
---
+++ Decrypter/frmMain.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Decrypter
+{
+    public partial class frmMain : Form
+    {
+        public frmMain(string Input = null, string Output = null)
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                tbInput.Text = OFD.FileName;
+            }
+        }
+
+        private void tbSave_Click(object sender, EventArgs e)
+        {
+            if (SFD.ShowDialog() == DialogResult.OK)
+            {
+                tbSave.Text = SFD.FileName;
+                if (!string.IsNullOrEmpty(tbOutput.Text))
+                {
+                    if (MessageBox.Show("Save the existing link list now?", "Save existing content", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        SaveList();
+                    }
+                }
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbInput.Text) && File.Exists(tbInput.Text))
+            {
+                byte[] Data = null;
+                try
+                {
+                    Data = File.ReadAllBytes(tbInput.Text);
+                }
+                catch(Exception ex)
+                {
+                    ShowError($"Can't read source file. Close other applications that might be using the file.\r\n\r\nMessage from system: {ex.Message}", "Source not readable");
+                    return;
+                }
+                var Result = ManualUpload.Upload(Data);
+                if (Result.success.links != null && Result.success.links.Length > 0)
+                {
+                    tbOutput.Lines = (string[])Result.success.links.Clone();
+                    SaveList();
+                }
+                else
+                {
+                    ShowError($"The API returned an empty result. Ensure the source file is really a link container. Message : {Result.success.message}", "API error");
+                }
+            }
+            else
+            {
+                ShowError("The source file could not be found", "Source not found");
+            }
+        }
+
+        private void SaveList()
+        {
+            if (!string.IsNullOrEmpty(tbOutput.Text))
+            {
+                if (!File.Exists(tbSave.Text) || MessageBox.Show("A file with this name already exists. Owerwrite it?", "Overwrite file", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.WriteAllLines(tbSave.Text, tbOutput.Lines);
+                    }
+                    catch (Exception ex)
+                    {
+                        ShowError($"Can't write file. Close other applications that might be using the file and ensure the media is not full or write protected.\r\n\r\nMessage from system: {ex.Message}", "Destination not writable");
+                    }
+                }
+            }
+        }
+
+        private void ShowError(string Message, string Title)
+        {
+            MessageBox.Show(Message, Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
