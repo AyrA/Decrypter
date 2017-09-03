f3cfdc64e2ebe8bcf350db7c2a4ea68a469ba3d6
Kevin Gut <kevin.gut@bitm.sg>
2017-09-03 14:58:43 +0200

    Implement generic decrypt interface (base)

---
+++ Decrypter/DecryptModules/Decrypter.cs
@@ -0,0 +1,17 @@
+﻿namespace Decrypter.DecryptModules
+{
+    public class Decrypter
+    {
+        public enum Mode : int
+        {
+            RSDF,
+            CCF,
+            DLC
+        }
+
+        public static string[] Decrypt(byte[] Content, Mode FileType)
+        {
+            return null;
+        }
+    }
+}
---
+++ Decrypter/Tools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Decrypter
+{
+    public static class Tools
+    {
+        public static byte[] ToByteArray(this string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                .ToArray();
+        }
+
+        public static string ToHexString(this IEnumerable<byte> B)
+        {
+            return string.Concat(B.Select(m => m.ToString("X2")));
+        }
+    }
+}
