21780c51fec166c9cd6ed8edc1dea13cfd2e595e
Kevin Gut <kevin.gut@bitm.sg>
2017-09-03 15:11:15 +0200

    Working Decryption component

--- Decrypter/DecryptModules/Decrypter.cs
+++ Decrypter/DecryptModules/Decrypter.cs
@@ -1,7 +1,21 @@
-﻿namespace Decrypter.DecryptModules
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace Decrypter.DecryptModules
 {
     public class Decrypter
     {
+        public struct WebResponse
+        {
+            public bool success;
+            public string message;
+            public string[] data;
+        }
+
+        public const string ENDPOINT = "https://cable.ayra.ch/decrypt/decrypt.php?mode=";
+
         public enum Mode : int
         {
             RSDF,
@@ -9,9 +23,39 @@
             DLC
         }
 
-        public static string[] Decrypt(byte[] Content, Mode FileType)
+        public static WebResponse Decrypt(byte[] Content, Mode FileType)
         {
-            return null;
+            var Req = WebRequest.CreateHttp(ENDPOINT + FileType.ToString().ToLower());
+
+            Req.Method = "POST";
+
+            using (var S = Req.GetRequestStream())
+            {
+                S.Write(Content, 0, Content.Length);
+            }
+            using (var Res = Req.GetResponse())
+            {
+                var Response = "";
+                try
+                {
+                    using (var S = Res.GetResponseStream())
+                    {
+                        using (var SR = new StreamReader(S))
+                        {
+                            return JsonConvert.DeserializeObject<WebResponse>(Response = SR.ReadToEnd());
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return new WebResponse()
+                    {
+                        data = new string[] { Response },
+                        message = "Can't process Web response. Message: " + ex.Message,
+                        success = false
+                    };
+                }
+            }
         }
     }
 }
